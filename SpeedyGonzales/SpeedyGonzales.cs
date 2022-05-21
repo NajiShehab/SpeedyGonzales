fb16787a272266e3816c6fa6bef08fafce699cda
Naji Shehab <naji.shehab@hotmail.com>
2022-05-21 11:34:21 +0100

    Initial setup

---
+++ SpeedyGonzales/SpeedyGonzales.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SpeedyGonzales : Mod
+{
+    public void Start()
+    {
+        Debug.Log("Mod SpeedyGonzales has been loaded!");
+    }
+
+    public void OnModUnload()
+    {
+        Debug.Log("Mod SpeedyGonzales has been unloaded!");
+    }
+}
